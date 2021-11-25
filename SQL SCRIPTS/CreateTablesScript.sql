e2280fc81e37457662b845a7e9a16f1d9849793d
Focus <maniaclord@bk.ru>
2021-11-25 21:33:02 +0300

    yes

--- SQL SCRIPTS/CreateTablesScript.sql
+++ SQL SCRIPTS/CreateTablesScript.sql
@@ -4,12 +4,12 @@ GO
 CREATE TABLE Users
 (
 	user_id INT NOT NULL IDENTITY,
-	password NVARCHAR(30) NOT NULL,
-	email NVARCHAR(30) NOT NULL,
-	name NVARCHAR(15) NOT NULL,
-	second_name NVARCHAR(15) NOT NULL,
+	password NVARCHAR(max) NOT NULL,
+	email NVARCHAR(max) NOT NULL,
+	first_name NVARCHAR(max) NOT NULL,
+	second_name NVARCHAR(max) NOT NULL,
 	register_date DATE NOT NULL,
-	avatar NVARCHAR(30), -- url to avatar img 
+	avatar_url NVARCHAR(max), -- url to avatar img 
 	CONSTRAINT pk_user PRIMARY KEY(user_id)
 )
 GO
@@ -18,17 +18,18 @@ GO
 CREATE TABLE Movies
 (
 	movie_id INT NOT NULL IDENTITY,
-	title NVARCHAR(30) NOT NULL, 
+	title NVARCHAR(max) NOT NULL, 
 	[length] INT NOT NULL check([length] >= 0),
+	genre NVARCHAR(max) NOT NULL,
 	realese_date DATE NOT NULL,
 	rating FLOAT NOT NULL default(0),
 	storyline TEXT NOT NULL,
-	director NVARCHAR(30) NOT NULL default('unknown'),
+	director NVARCHAR(max) NOT NULL default('unknown'),
 	budget INT NOT NULL check(budget >= 0),
-	country NVARCHAR(30) NOT NULL,
-	[language] NVARCHAR(30) NOT NULL,
-	company NVARCHAR(30) NOT NULL default('unknown'),
-	cover NVARCHAR(30), -- url to cover img 
+	country NVARCHAR(max) NOT NULL,
+	[language] NVARCHAR(max) NOT NULL,
+	company NVARCHAR(max) NOT NULL default('unknown'),
+	poster_url NVARCHAR(max), -- url to poster img 
 
 
 	CONSTRAINT pk_movie PRIMARY KEY(movie_id),
@@ -41,7 +42,7 @@ CREATE TABLE Reviews
 	review_id INT NOT NULL IDENTITY,
 	user_id INT NOT NULL, --FK (poka xz kak imenno)
 	movie_id INT NOT NULL, --FK (poka xz kak imenno)
-	text TEXT NOT NULL,
+	[text] TEXT NOT NULL,
 	review_date DATE NOT NULL, 
 	rating INT NOT NULL DEFAULT(0),
 
@@ -52,5 +53,18 @@ CREATE TABLE Reviews
 	CONSTRAINT fk_review_movie FOREIGN KEY(movie_id)
 		REFERENCES Movies(movie_id)
 		on delete cascade on update cascade
+)
+GO
+
+GO
+CREATE TABLE News
+(
+	news_id INT NOT NULL IDENTITY,
+	poster_url NVARCHAR(max), -- url to poster img 
+	title NVARCHAR(max) NOT NULL, 
+	news_date DATE NOT NULL,
+	[text] TEXT NOT NULL
+
+	CONSTRAINT pk_news PRIMARY KEY(news_id)
 )
 GO
