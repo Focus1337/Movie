3c42332f12508729106429f7df95132af0582e21
Focus <maniaclord@bk.ru>
2021-11-26 21:08:29 +0300

    test web api

---
+++ BACK/WebAPI/WebAPI/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
